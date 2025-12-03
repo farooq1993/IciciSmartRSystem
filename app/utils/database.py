768813e9192c97b2f42971f898b45d03729204ad
=farooq <=farooqaziz1993@gmail.com>
2025-12-03 13:38:49 +0530

    change database type

--- app/utils/database.py
+++ app/utils/database.py
@@ -11,7 +11,8 @@ DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 DATABASE_PORT = os.getenv('DATABASE_PORT', 5432)
 DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
 
-DATABASE_URL = f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+DATABASE_URL = "sqlite:///icici.db"
+#f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 #"sqlite:///icici.db"
 
 engine = create_engine(DATABASE_URL, echo=False) 
